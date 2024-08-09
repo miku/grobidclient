3eaf5cd788237ceeff870680f132aa5ce4ca67ef
Martin Czygan <martin.czygan@gmail.com>
2024-08-09 14:38:15 +0200

    reorganize test data

--- client_test.go
+++ client_test.go
@@ -1,12 +1,65 @@
 package grobidclient
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"os/user"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
+
+func TestProcessPDF(t *testing.T) {
+	skipNoDocker(t)
+	if testing.Short() {
+		t.Skip("skipping testcontainer based tests in short mode")
+	}
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "grobid/grobid:0.8.0",
+		ExposedPorts: []string{"8070/tcp"},
+		WaitingFor:   wait.ForListeningPort("8070/tcp"),
+	}
+	grobidC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Fatalf("Could not start grobid: %s", err)
+	}
+	defer func() {
+		if err := grobidC.Terminate(ctx); err != nil {
+			t.Fatalf("Could not stop grobid: %s", err)
+		}
+	}()
+	ip, err := grobidC.Host(ctx)
+	if err != nil {
+		t.Fatalf("TC: count not get host: %v", err)
+	}
+	port, err := grobidC.MappedPort(ctx, "8070")
+	if err != nil {
+		t.Fatalf("TC: count not get port: %v", err)
+	}
+	hostPort := fmt.Sprintf("http://%s:%s", ip, port.Port())
+	t.Logf("starting e2e test, using grobid container running at %v", hostPort)
+	grobid := New(hostPort)
+	result, err := grobid.ProcessPDF(
+		"testdata/pdf/062RoisinAronAmericanNaturalist03.pdf",
+		"processFulltextDocument",
+		nil)
+	if err != nil {
+		t.Fatalf("expected successful parse, got %v", err)
+	}
+	if result.StatusCode != 200 {
+		t.Fatalf("expected HTTP 200, got %v: %v", result.StatusCode, string(result.Body))
+	}
+}
 
 func TestParseLines(t *testing.T) {
 	var cases = []struct {
@@ -119,5 +172,39 @@ func TestDefaultResultWriter(t *testing.T) {
 				os.Remove(c.dst)
 			}
 		}
+	}
+}
+
+func skipNoDocker(t *testing.T) {
+	noDocker := false
+	cmd := exec.Command("systemctl", "is-active", "docker")
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		noDocker = true
+	}
+	if strings.TrimSpace(string(b)) != "active" {
+		noDocker = true
+	}
+	if !noDocker {
+		// We found some docker.
+		return
+	}
+	// Otherwise, try podman.
+	_, err = exec.LookPath("podman")
+	if err == nil {
+		t.Logf("podman detected")
+		// DOCKER_HOST=unix:///run/user/$UID/podman/podman.sock
+		usr, err := user.Current()
+		if err != nil {
+			t.Logf("cannot get UID, set DOCKER_HOST manually")
+		} else {
+			sckt := fmt.Sprintf("unix:///run/user/%v/podman/podman.sock", usr.Uid)
+			os.Setenv("DOCKER_HOST", sckt)
+			t.Logf("set DOCKER_HOST to %v", sckt)
+		}
+		noDocker = false
+	}
+	if noDocker {
+		t.Skipf("docker not installed or not running")
 	}
 }
